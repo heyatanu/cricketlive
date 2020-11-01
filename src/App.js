038748aa8fbaa38af0c9a75705e56b59ce8ae498
heyatanu <atanud662@gmail.com>
2020-11-01 21:51:51 +0530

    New Updatee

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import Apifetch from './RecentGameApifetch';
 import UpcomeingGame from './UpcomeingGame';
 import TodayMatch from './TodayMatch';
 import LiveMatch from './LiveMatch';
-import {Route,Switch,Redirect} from 'react-router-dom';
-function Er(){
-  return(<h1>EEERRROOOORRRR</h1>)
-}
+import {Route,Switch} from 'react-router-dom';
+
 
 function App() {
   return (
     <>
      <Switch>
        <Route exact path="/" component={LiveMatch}  />
-       <Route exact path="cricketlive/upcomeing" component={UpcomeingGame}  />
-       <Route  exact path="cricketlive/todaymatch" component={TodayMatch}  />
-       <Route exact path="cricketlive/recentmatch" component={Apifetch}  />
-       <Redirect to="/" />
+       <Route exact path="/upcomeing" component={UpcomeingGame}  />
+       <Route  exact path="/todaymatch" component={TodayMatch}  />
+       <Route exact path="/recentmatch" component={Apifetch}  />
+       <Route component={LiveMatch} />
       
      </Switch>
     
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,8 +9,7 @@ function Navbar(){
         <li><NavLink exact activeClassName="active" to="/">Watch Live Matches</NavLink></li>
         <li><NavLink exact activeClassName="active" to="/upcomeing">Watch Upcomeing Matches Schedule</NavLink></li>
         <li><NavLink exact activeClassName="active" to="/todaymatch">Watch Today Matches Schedule</NavLink></li>
-        <li><NavLink exact activeClassName="active" to="/recentmatch">Watch Recent Matches</NavLink></li>
-        </ul>
+        <li><NavLink exact activeClassName="active" to="/recentmatch">Watch Recent Matches</NavLink></li></ul>
     </>);
 }
 
