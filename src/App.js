c872ee51a8e2d148fd2c7b4761bf55d2c6fbe7f6
heyatanu <atanud662@gmail.com>
2020-11-01 17:47:36 +0530

    cricket live score app

--- src/App.js
+++ src/App.js
@@ -6,21 +6,19 @@ import TodayMatch from './TodayMatch';
 import LiveMatch from './LiveMatch';
 import {Route,Switch} from 'react-router-dom';
 
-function EOORr(){
-  return(<p>Error</p>)
-}
+
 function App() {
   return (
     <>
      <Switch>
-       <Route exact  path="/" component={LiveMatch}  />
+       <Route exact path="/" component={LiveMatch}  />
        <Route exact path="/upcomeing" component={UpcomeingGame}  />
-       <Route exact path="/todaymatch" component={TodayMatch}  />
+       <Route  exact path="/todaymatch" component={TodayMatch}  />
        <Route exact path="/recentmatch" component={Apifetch}  />
-       <Route component={EOORr} />
+       <Route component={LiveMatch} />
       
      </Switch>
-
+    
     </>
   );
 }
